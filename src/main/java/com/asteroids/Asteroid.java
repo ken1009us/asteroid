de852786ffa849c06c366740477797e5270ccff7
Ken Wu <kenwu1009us@gmail.com>
2023-09-21 21:57:38 -0700

    make asteroids move

--- src/main/java/com/asteroids/Asteroid.java
+++ src/main/java/com/asteroids/Asteroid.java
@@ -2,8 +2,28 @@ package com.asteroids;
 
 import javafx.scene.shape.Polygon;
 
+import java.util.Random;
+
 public class Asteroid extends Character {
+    private double rotationalMovement;
     public Asteroid(int x, int y) {
         super(new PolygonFactory().createPolygon(), x, y);
+
+        Random rand = new Random();
+
+        super.getCharacter().setRotate(rand.nextInt(360));
+
+        int accelerationAmount = 1 + rand.nextInt(10);
+        for (int i = 0; i < accelerationAmount; i++) {
+            accelerate();
+        }
+
+        this.rotationalMovement = 0.5 - rand.nextDouble();
+    }
+
+    @Override
+    public void move() {
+        super.move();
+        this.getCharacter().setRotate(super.getCharacter().getRotate() + rotationalMovement);
     }
 }
