3586c54b03deef5e724b501ee9dad35fa1234155
Ken Wu <kenwu1009us@gmail.com>
2023-09-21 20:48:55 -0700

    change the shape of the ship

--- src/main/java/com/asteroids/Ship.java
+++ src/main/java/com/asteroids/Ship.java
@@ -4,6 +4,6 @@ import javafx.scene.shape.Polygon;
 
 public class Ship extends Character {
     public Ship(int x, int y) {
-        super(new Polygon(-10, -10, 20, 0, -10, 10), x ,y);
+        super(new Polygon( 20, 0, -10, 10, -4, 5, -4, -5, -10, -10), x ,y);
     }
 }
