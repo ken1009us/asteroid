7ed21451d0ef80633148a217c31f65cb269f9a6a
Ken Wu <kenwu1009us@gmail.com>
2023-09-21 20:50:08 -0700

    multiple asteroids

--- src/main/java/com/asteroids/Asteroid.java
+++ src/main/java/com/asteroids/Asteroid.java
@@ -4,6 +4,6 @@ import javafx.scene.shape.Polygon;
 
 public class Asteroid extends Character {
     public Asteroid(int x, int y) {
-        super(new Polygon(20, -20, 20, 20, -20, 20, -20, -20), x, y);
+        super(new PolygonFactory().createPolygon(), x, y);
     }
 }
